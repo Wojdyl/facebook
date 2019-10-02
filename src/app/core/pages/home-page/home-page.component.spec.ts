b668d959891e0b3a011fd784588e9886a6622540
Patryk Wojdyla <patryk.wojdyla.com>
2019-10-02 14:42:36 +0200

    spec

--- src/app/core/pages/home-page/home-page.component.spec.ts
+++ src/app/core/pages/home-page/home-page.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePageComponent } from './home-page.component';
+import {MenuComponent} from '../../components/menu/menu.component';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let $component = null;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,7 +18,12 @@ describe('HomePageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
+    $component = fixture.nativeElement;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    $component.remove();
   });
 
   it('should create', () => {
