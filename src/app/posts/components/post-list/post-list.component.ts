36aa2b0cc12bd3b4d402eb8a5a0af5b6cb9d11b4
Patryk Wojdyla <patryk.wojdyla.com>
2019-10-02 15:06:58 +0200

    post-list and post-list-item

---
+++ src/app/posts/components/post-list/post-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit {
+  posts = [
+    { id: '123123dasdas', body: 'asdf' },
+    { id: '123123das12312das', body: 'qwert' },
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
