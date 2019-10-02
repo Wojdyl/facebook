675335ce1c1a8383ddbb7f654e57ea06c8e16f1b
Patryk Wojdyla <patryk.wojdyla.com>
2019-10-02 15:24:30 +0200

    new directive highlight

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { PostListItemComponent } from './components/post-list-item/post-list-ite
   ],
   imports: [
     CommonModule,
-    PostsRoutingModule
+    PostsRoutingModule,
+    SharedModule
   ]
 })
 export class PostsModule { }
