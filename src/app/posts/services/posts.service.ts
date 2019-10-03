25b0d42921ead5192a43d6da85e1d0efa85e30a6
Patryk Wojdyla <patryk.wojdyla.com>
2019-10-03 14:24:23 +0200

    sort

--- src/app/posts/services/posts.service.ts
+++ src/app/posts/services/posts.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {IPostList} from '../../shared/interfaces/post-list.interface';
+import {sortByOperator} from '../../shared/helpers/sorter.helper';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
-  constructor(private http: HttpClient) {
-  }
-  getPosts() {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  async getPosts() {
     const url = 'assets/fake-posts.json';
-    return this.http.get<IPostList>(url).toPromise();
+    const response = await this.http.get<IPostList>(url)
+      .pipe(
+        sortByOperator('createdTime')
+      )
+      .toPromise();
+
+    return response;
   }
 }
---
+++ src/app/shared/helpers/sorter.helper.ts
@@ -0,0 +1,25 @@
+import {map} from 'rxjs/operators';
+import {IPostList} from '../interfaces/post-list.interface';
+
+export function sortBy(collection, mapTo) {
+  return collection.sort((item1, item2) => {
+    const date1 = mapTo(item1);
+    const date2 = mapTo(item2);
+    if (date1 < date2) {
+      return 1;
+    } else if (date1 > date2) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
+
+
+export function sortByOperator ( key ) {
+  return map((list) => {
+    return sortBy(list, (item) => {
+      return new Date(item[key]);
+    });
+  })
+}
